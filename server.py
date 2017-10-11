c010cb69170e0fe6bbddc465a50105e9510519e3
Brenton Durkee <brentdur@gmail.com>
2017-10-11 17:26:06 -0400

    Add fixtures and tests for 404, 403

--- server.py
+++ server.py
@@ -12,7 +12,13 @@ PORT = os.getenv('PORT', '5000')
 @app.route('/wishlists/<int:wishlist_id>', methods=['DELETE'])
 def delete_wishlist(wishlist_id):
     """ Deletes the wishlist with the provided id""" 
-    return make_response('Wishlist {} deleted'.format(wishlist_id))
+    # TODO undo these fixtures once persistance is added
+    if wishlist_id == 3:
+        return make_response('Wishlist {} deleted'.format(wishlist_id))
+    elif wishlist_id == 2:
+        return make_response('Wishlist does not exist', status.HTTP_404_NOT_FOUND)
+    else:
+        return make_response('Wishlist does not exist', status.HTTP_403_FORBIDDEN)
 
 if __name__ == "__main__":
     print "Wishlist Service Starting..."
