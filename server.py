257c660b230a5d86901e50ff46cf88205afd703c
DB3619 <db3619@nyu.edu>
2017-11-29 03:00:39 +0000

    added persistence to all models and also cleaned code to include hw1 suggestions

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request, url_for, make_response
 from flask_api import status
 import sys
 from werkzeug.exceptions import NotFound
-from model import CustomerList, DataValidationError, Customer
+from model import Customer, DataValidationError, Wishlist
 
 # Create Flask application
 app = Flask(__name__)
@@ -29,20 +29,14 @@ def bad_request(error):
     app.logger.info(message)
     return jsonify(status=400, error='Bad Request', message=message), 400
 
-@app.route('/', methods=['GET'])
-def home_page():
-    return "WELCOME TO WISHLIST PAGE"
 ######################################################
 ########                DELETE                ########
 ######################################################
 @app.route('/wishlists/<int:cust_id>/<int:wishlist_id>', methods=['DELETE'])
 def delete_wishlist(cust_id, wishlist_id):
     """ Deletes the wishlist with the provided id"""
-    success = CustomerList.delete_by_id(cust_id, wishlist_id)
-    if success:
-        return make_response('Wishlist {} deleted'.format(wishlist_id))
-    else:
-        return make_response('Wishlist does not exist', status.HTTP_404_NOT_FOUND)
+    success = Customer.delete_by_id(cust_id, wishlist_id)
+    return make_response('', status.HTTP_204_NO_CONTENT)
 
 ######################################################
 ########              POST/CREATE             ########
@@ -50,11 +44,11 @@ def delete_wishlist(cust_id, wishlist_id):
 @app.route('/wishlists/<int:cust_id>', methods=['POST'])
 def create_wishlist(cust_id):
     """ create the wishlist with the provided id"""
-    wishlist = CustomerList(cust_id, '')
+    wishlist = Customer(cust_id,"",[])
     wishlist.deserialize(request.get_json())
-    wishlist.save()
-    message = wishlist.serialize()
-    location_url = url_for('create_wishlist', cust_id=wishlist.id)
+    message = wishlist.save()
+
+    location_url = url_for('create_wishlist', cust_id=wishlist.cust_id)
     return make_response(jsonify(message), status.HTTP_201_CREATED,
                          {
                              'Location': location_url
@@ -68,34 +62,57 @@ def create_wishlist(cust_id):
 def update_wishlist(cust_id,wishlist_id):
     """ Updates the wishlist name if it exists, otherwise returns not found """
     # TODO add products changes as well, for now just asses the wishlists
-    wishlists = CustomerList.find_by_id(cust_id,wishlist_id)
-    if wishlists:
-        old_name = wishlists['Wishlist name']
-        message = CustomerList.update(request.get_json(),old_name,cust_id)
-        return make_response(jsonify(message), status.HTTP_200_OK)
+    if Customer.check_custid(cust_id):
+        message = Customer.find_by_id(cust_id,wishlist_id)
+        if message:
+            result = Customer.update(cust_id,wishlist_id,request.get_json())
+            res = Customer.find_by_id(cust_id,wishlist_id)
+            return make_response(jsonify(res), status.HTTP_200_OK)
+        else:
+            message = {'Error': 'Wishlist with given ID not found'}
+            return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
     else:
-        message = {'Error': 'Wishlist not found'}
+        message = {'Invalid' : 'Invalid customer ID'}
         return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
 
 ######################################################
 ########               PUT/ADD A PRODUCT      ########
 ######################################################
-@app.route('/wishlists/<int:cust_id>/<int:wishlist_id>/add/<int:pid>', methods=['PUT', 'PATCH'])
+@app.route('/wishlists/<int:cust_id>/<int:wishlist_id>/<int:pid>', methods=['PUT', 'PATCH'])
 def add_product(cust_id,wishlist_id,pid):
     """ Add product ID to a wishlist """
     # TODO add products changes as well, for now just asses the wishlists
-    wishlists = CustomerList.find_by_id(cust_id,wishlist_id)
-    if wishlists:
-        product = wishlists['Product list']
-        name = wishlists['Wishlist name']
-        product.append(pid)
-        c = CustomerList(cust_id,name)
-        c.pid = pid
-        c.save()
-        message = c.serialize()
-        return make_response(jsonify(message), status.HTTP_200_OK)
+    if Customer.check_custid(cust_id):
+        message = Customer.find_by_id(cust_id,wishlist_id)
+        if message:
+            result = Customer.addProduct(cust_id,wishlist_id,pid)
+            res = Customer.find_by_id(cust_id,wishlist_id)
+            return make_response(jsonify(res), status.HTTP_200_OK)
+        else:
+            message = {'Error': 'Wishlist with given ID not found'}
+            return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
     else:
-        message = {'Error': 'Wishlist not found'}
+        message = {'Invalid' : 'Invalid customer ID'}
+        return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
+
+######################################################
+########               Delete A PRODUCT      ########
+######################################################
+@app.route('/wishlists/<int:cust_id>/<int:wishlist_id>/<int:pid>', methods=['DELETE'])
+def delete_product(cust_id,wishlist_id,pid):
+    """ delete product ID to a wishlist """
+    # TODO add products changes as well, for now just asses the wishlists
+    if Customer.check_custid(cust_id):
+        message = Customer.find_by_id(cust_id,wishlist_id)
+        if message:
+            result = Customer.deleteProduct(cust_id,wishlist_id,pid)
+            res = Customer.find_by_id(cust_id,wishlist_id)
+            return make_response(jsonify(res), status.HTTP_200_OK)
+        else:
+            message = {'Error': 'Wishlist with given ID not found'}
+            return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
+    else:
+        message = {'Invalid' : 'Invalid customer ID'}
         return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
 
 ##########################################################################
@@ -104,27 +121,23 @@ def add_product(cust_id,wishlist_id,pid):
 @app.route('/wishlists/<int:cust_id>' , methods=['GET'])
 def query_wishlist(cust_id):
     """ List the wishlist with the provided name"""
-
-    wishlists = CustomerList.find(cust_id)
-    wishlist_name = request.args.get('query')
-    if wishlist_name:
-        if wishlists:
-            message = CustomerList.find_wishlist(wishlists,wishlist_name,cust_id)
+    if Customer.check_custid(cust_id):
+        query_name = request.args.get('name')
+        if query_name:
+            message = Customer.find_by_name(cust_id,query_name)
             if message:
                 return make_response(jsonify(message),status.HTTP_200_OK)
             else:
                 message = {'Error' : 'Wishlist with the given name not found'}
                 return make_response(jsonify(message),status.HTTP_404_NOT_FOUND)
         else:
-            message = {'No Content' : 'Wishlist Empty'}
-            return make_response(jsonify(message),status.HTTP_204_NO_CONTENT)
+            message = Customer.find_by_custid(cust_id)
+            return make_response(jsonify(message),status.HTTP_200_OK)
     else:
-        if wishlists:
-            message = [CustomerList.find_wishlist(wishlists,w,cust_id) for w in wishlists]
-            return make_response(jsonify(message), status.HTTP_200_OK)
-        else:
-            message = {'No Content' : 'Wishlist Empty'}
-            return make_response(jsonify(message), status.HTTP_204_NO_CONTENT)
+        message = {'Invalid' : 'Invalid customer ID'}
+        return make_response(jsonify(message), status.HTTP_204_NO_CONTENT)
+
+
 
 #########################################################################
 ########                GET/SEE  a Specific Wishlist             ########
@@ -132,40 +145,62 @@ def query_wishlist(cust_id):
 @app.route('/wishlists/<int:cust_id>/<int:wishlist_id>' , methods=['GET'])
 def get_wishlist(cust_id,wishlist_id):
     """ List the wishlist with the provided name"""
-
-    wishlists = CustomerList.find_by_id(cust_id,wishlist_id)
-
-    if wishlists:
-        return make_response(jsonify(wishlists),status.HTTP_200_OK)
+    if Customer.check_custid(cust_id):
+        message = Customer.find_by_id(cust_id,wishlist_id)
+        if message:
+            return make_response(jsonify(message),status.HTTP_200_OK)
+        else:
+            message = {'Error' : 'Wishlist with the given ID not found'}
+            return make_response(jsonify(message),status.HTTP_404_NOT_FOUND)
     else:
-        message = {'Error' : 'Wishlist with the given ID not found'}
-        return make_response(jsonify(message),status.HTTP_404_NOT_FOUND)
+        message = {'Invalid' : 'Invalid customer ID'}
+        return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
+
+#########################################################################
+########                Clear  a Specific Wishlist               ########
+#########################################################################
 
 @app.route('/wishlists/<int:cust_id>/<int:wishlist_id>/clear' , methods=['PUT','PATCH'])
 def clear_wishlist(cust_id,wishlist_id):
     """ Clear the contents of the wishlist with the given id"""
-
-    wishlists = CustomerList.find_by_id(cust_id,wishlist_id)
-
-    if wishlists:
-        message = CustomerList.clear_list(cust_id,wishlists)
-        return make_response(jsonify(message),status.HTTP_200_OK)
+    if Customer.check_custid(cust_id):
+        message = Customer.find_by_id(cust_id,wishlist_id)
+        if message:
+            res = Customer.clear_list(cust_id,wishlist_id)
+            return make_response(jsonify(message),status.HTTP_200_OK)
+        else:
+            message = {'Error' : 'Wishlist with the given ID not found'}
+            return make_response(jsonify(message),status.HTTP_404_NOT_FOUND)
     else:
-        message = {'Error' : 'Wishlist with the given ID not found'}
-        return make_response(jsonify(message),status.HTTP_404_NOT_FOUND)
+        message = {'Invalid' : 'Invalid customer ID'}
+        return make_response(jsonify(message), status.HTTP_404_NOT_FOUND)
+
+#########################################################################
+########                Display all wishlists                    ########
+#########################################################################
 
 @app.route('/wishlists' , methods=['GET'])
 def display_all_wishlists():
     """ Display wishlists of all customers if created"""
 
-    customers = CustomerList.display_all()
-
-    if customers:
-        message = [{"Customer ID " : custid , "Wishlists " : [CustomerList.find_wishlist(CustomerList.find(custid),w,custid) for w in CustomerList.find(custid)]} for custid in customers]
+    if Customer.display_all():
+        message = [Customer.find_by_custid(k) for k in Customer.redis.keys()]
         return make_response(jsonify(message),status.HTTP_200_OK)
     else:
         message = {'Error' : 'No wishlist created for any customer'}
         return make_response(jsonify(message),status.HTTP_404_NOT_FOUND)
+
+@app.before_first_request
+def init_db(redis=None):
+    """ Initlialize the model """
+    Customer.init_db(redis)
+
+def data_reset():
+    Customer.remove_all()
+
+
+
+
 
 if __name__ == "__main__":
     print "Wishlist Service Starting..."
